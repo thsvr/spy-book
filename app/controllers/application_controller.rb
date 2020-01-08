4859e260b248282d605b2d04799adaaa85c42e5b
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-01-08 16:12:28 -0500

    added post model and created associations between posts and users. added post controller and modified user controller. added post views and user views. added some visual styling to some views and navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-        def configure_permitted_parameters
-            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:fname, :lname, :email, :password) }
-            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:fname, :lname, :email, :password, :current_password, :is_female, :date_of_birth) }
-        end
-end 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :lname, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys:
+                                      %i[fname lname email password current_password is female date_of_birth])
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
-    def index
-    end
+  def index
+    @users = User.all
+    @posts = Post.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
