27ff450a024be020e58dbed23f9f9ea1b699987a
thsvr <vr.ths.zd@gmail.com>
2020-01-08 14:37:48 -0300

    created devise user model - signup, sign-in, logout. created users controller and the registration view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :authenticate_user!
+
+    before_filter :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:fname, :lname, :email, :password) }
+            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:fname, :lname, :email, :password, :current_password, :is_female, :date_of_birth) }
+        end
+end 
