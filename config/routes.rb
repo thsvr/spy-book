e34685f31e6b40124d0fd567afbdfea5f2f5afa2
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-01-10 13:40:24 -0500

    added likes model and controller. added ability to like and dislike both comments and posts. edited posts index view to show all posts. edited user index view to show all users and a friendrequest button.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-  get 'comments/create'
   root 'users#index'
   devise_for :users
-  resources :posts, only: %i[new create show destroy]
   resources :users, only: %i[index show]
-  resources :comments, only: %i[new create destroy]
+  resources :posts, only: %i[index new create show destroy] do
+      resources :likes, only: %i[create]
+  end
+  resources :comments, only: %i[new create destroy] do
+    resources :likes, only: %i[create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
