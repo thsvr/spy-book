f91c18051fcfa72d5d08f6c041e8897844bdd793
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-01-12 15:27:31 -0500

    added modal for decisions accept and decline. added ability to accept or decline friendship requests. added routes for accepting and declining friendship requests. added helper methods, request friendRequestSent?, friendRequestRecieved? and friendRequestStatus? , to check the existence of friendship status and requests. edited users index view to show status of friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,7 +3,7 @@ class FriendshipsController < ApplicationController
   def create
     @friendrequest = current_user.friendsent.build(sent_to_id: params[:user_id])
     if @friendrequest.save
-      flash[:sucess] = "Friend Request sent!"
+      flash[:success] = "Friend Request sent!"
     else
       flash[:danger] = "Friend Request could not be sent!"
     end
@@ -11,19 +11,23 @@ class FriendshipsController < ApplicationController
   end
 
   def acceptFriend
-    @friendrequest = Friendship.find(params[:id])
-    if @friendrequest.sent_to_id == current_user.id
+    @friendrequest = Friendship.find_by(sent_by_id: params[:user_id], sent_to_id: current_user.id)
+    if @friendrequest
       @friendrequest.status = true
-      flash[:sucess] = "Friend Request Accepted!"
-      redirect_back(fallback_location: root_path)
+      if @friendrequest.save
+        flash[:success] = "Friend Request Accepted!"
+        redirect_back(fallback_location: root_path)
+      else
+        flash[:danger] = "Friend Request could not be accepted!"
+      end
     end
   end
 
-  def destroy
-    @friendrequest = Friendship.find(params[:id])
+  def declineFriend
+    @friendrequest = Friendship.find_by(sent_by_id: params[:user_id], sent_to_id: current_user.id)
 
     @friendrequest.destroy
-    flash[:success] = 'Removed friend'
+    flash[:warning] = 'Successfully Removed friend'
     redirect_back(fallback_location: root_path)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,6 +14,25 @@ module ApplicationHelper
   # Checks whether or not a user has had a friend request sent to them by the current user
   # returning either true or false
   def friendRequestSent?(user)
-    current_user.friendsent.exists?(sent_to_id: user.id)
+    requestSent = current_user.friendsent.exists?(sent_to_id: user.id, status: false)
+  end
+
+  # Checks whether or not a user has sent a friend request to the current user
+  # returning either true or false
+  def friendRequestRecieved?(user)
+    current_user.friendrequests.exists?(sent_by_id: user.id, status: false)
+  end
+
+  # Checks whether or not a user has had a friend request sent to them by the current user
+  # or if the current user has been sent a friend request by the user 
+  # and the reciever has accepted the friendship
+  # returning either true or false
+  def friendRequestStatus?(user)
+    requestSent = current_user.friendsent.exists?(sent_to_id: user.id, status: true)
+    requestRecieved = current_user.friendrequests.exists?(sent_by_id: user.id, status: true)
+
+    return true if requestSent != requestRecieved 
+    return true if requestSent == requestRecieved && requestSent == true
+    return false if requestSent == requestRecieved && requestSent == false
   end
 end
