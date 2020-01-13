440121fcd9280b3e8d1483143fa59f2d4207d399
thsvr <vr.ths.zd@gmail.com>
2020-01-13 19:10:46 -0300

    added the friendship model, created the associations between users and friendships model - linked by foreign keys, sent_by_id and sent_to_id

---
+++ app/models/friendship.rb
@@ -0,0 +1,5 @@
+class Friendship < ApplicationRecord
+  belongs_to :user, foreign_key:"sent_by_id"
+  belongs_to :user, foreign_key:"sent_to_id"
+  scope :friends, -> { where('status =?', true)}
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friend_sent, :class_name => 'Friendship', foreign_key:"sent_by_id"
+  has_many :friend_request, :class_name => 'Friendship', foreign_key:"sent_to_id"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
