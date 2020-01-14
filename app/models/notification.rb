fea0cd979b3c39cb98066b916a47f9914aba07c2
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-01-14 18:11:17 -0500

    rubocop fixes

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 class Notification < ApplicationRecord
   belongs_to :user
   scope :friend_requests, -> { where('type = friendRquest') }
   scope :likes, -> { where('type = like') }
   scope :comments, -> { where('type = comment') }
-
 end
