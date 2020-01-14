34152d54fd56c1386769ea76c9b9f7982b9838f5
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-01-14 10:58:04 -0500

    added ability to accept and decline friendship requests, necessary routes and modified user index view to show changes in relationship between users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :friend_sent, :class_name => 'Friendship', foreign_key:"sent_by_id"
-  has_many :friend_request, :class_name => 'Friendship', foreign_key:"sent_to_id"
+  has_many :friend_sent, class_name: 'Friendship', foreign_key:"sent_by_id", dependent: :destroy
+  has_many :friend_request, class_name: 'Friendship', foreign_key:"sent_to_id", dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
   resources :users, only: %i[index show] do
-    resources :friendships, only: %i[create]
+    resources :friendships, only: %i[create] do
+      collection do
+        get 'accept_friend'
+        get 'decline_friend'
+      end
+    end
   end
 
   resources :posts, only: %i[index new create show destroy] do
