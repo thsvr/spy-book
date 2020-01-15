6273953a2efe02e831798e6913ee7e942f506761
thsvr <vr.ths.zd@gmail.com>
2020-01-15 20:01:29 -0300

    modified views - sign in, signup and users page - added style to them

--- db/schema.rb
+++ db/schema.rb
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 2020_01_15_170437) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "notice_seen", default: false
+    t.boolean "notice_seen", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
