a4ef360a78edbbd57af184a235f934afa8f4c120
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-01-16 15:26:20 -0500

    added some style changes from design branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friend_sent, class_name: 'Friendship', foreign_key: 'sent_by_id', inverse_of: 'sent_by', dependent: :destroy
-  has_many :friend_request, class_name: 'Friendship', foreign_key: 'sent_to_id', inverse_of: 'sent_to', dependent: :destroy
+  has_many :friend_request, class_name: 'Friendship', foreign_key: 'sent_to_id',
+                            inverse_of: 'sent_to', dependent: :destroy
   has_many :notifications, dependent: :destroy
   mount_uploader :image, PictureUploader
   # Include default devise modules. Others available are:
