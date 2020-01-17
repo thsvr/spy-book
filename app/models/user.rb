b762c65f751c3b595de37658269f9e81f9577b8d
thsvr <vr.ths.zd@gmail.com>
2020-01-17 13:55:33 -0300

    added omniauth facebook authentication allowing the users to sign up and login with their facebook accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[facebook]
   validates :fname, length: { in: 3..15 }, presence: true
   validates :lname, length: { in: 3..15 }, presence: true
   validate :picture_size
@@ -68,6 +69,27 @@ class User < ApplicationRecord
     friend_requests
   end
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.fname = auth.info.first_name   # assuming the user model has a first name
+      user.lname = auth.info.last_name   # assuming the user model has a last name
+      user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+  
   private
 
   # Validates the size of an uploaded picture.
